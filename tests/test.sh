1d42ea9682e0edd3a4c0548c1c59a36c75bf2683
Bertrand Janin <b@janin.com>
2017-07-01 17:11:50 -0400

    Decode + as spaces, also wrap the response since it can't trigger a bot.

---
+++ tests/test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+curl \
+	--data channel_id=hawos4dqtby53pd64o4a4cmeoo \
+	--data channel_name=town-square \
+	--data team_domain=someteam \
+	--data team_id=kwoknj9nwpypzgzy78wkw516qe \
+	--data post_id=axdygg1957njfe5pu38saikdho \
+	--data text=rogy:+some+text+here \
+	--data timestamp=1445532266 \
+	--data token=zmigewsanbbsdf59xnmduzypjc \
+	--data trigger_word=some \
+	--data user_id=rnina9994bde8mua79zqcg5hmo \
+	--data user_name=somename \
+	http://localhost:3333/
